456ca6f696b827b8692bcbaf1e930ce52bfaadc5
tanviranik004 <tanvir.anik.009@gmail.com>
2020-08-25 20:01:49 +0600

    comment

---
+++ comment.py
@@ -0,0 +1,10 @@
+#This is Single Line Comment;
+
+'''
+    This is a multiple line Comment;
+    Python is my favorite programming language
+'''
+
+print("Hello Python")
+print("I am Tanvir Anik")
+print("This Is my Phone Number: 0099456777 ")
